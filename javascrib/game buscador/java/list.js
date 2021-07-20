5a6c858f54c4c0829bb62f0eaef485bc26ab63da
skenda_pc <skenda.computers@gmail.com>
2021-07-20 11:25:09 -0500

    termino de las condiciones

--- javascrib/game buscador/java/list.js
+++ javascrib/game buscador/java/list.js
@@ -2,9 +2,35 @@ let getRandonNumber = size => {
     return math.floor[RandonNumber() * size]
 
 }
-let getdistance = (e, objetivo) => 
-{
+let getdistance = (e, objetivo) => {
     let difx = e.offsetx - objetivo.x;
     let dify = e.offsety - objetivo.y;
-    return math.sqrt((difx * difx) + (dify - dify));
+    return math.sqrt((difx * difx) + (dify + dify));
+
+}
+let getdistance = distance => {
+    if (distance < 30) {
+        return "Se Quemo"
     }
+    else if (distance < 50) {
+        return "Muy Caliente"
+    }
+    else if (distance < 80) {
+        return "Caliente"
+    }
+    else if (distance < 100) {
+        return "Tibio"
+    }
+    else if (distance < 130) {
+        return "frio"
+    }
+    else if (distance < 180) {
+        return "Muy frio"
+    }
+    else if (distance < 360) {
+        return "HIELO "
+    }
+    
+    else {
+    return "CONGELADO"
+}
