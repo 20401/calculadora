73fdcaab4b9a12ca4a44e7b2e5f71d38c45d86bd
skenda_pc <skenda.computers@gmail.com>
2021-07-20 09:38:26 -0500

    studiocade 3
    Signed-off-by: skenda_pc <skenda.computers@gmail.com>

--- javascrib/game buscador/java/list.js
+++ javascrib/game buscador/java/list.js
@@ -1,10 +1,10 @@
-let getRandonNumber = size ==> {
-    return math.floor[RandonNumber() * size];
+let getRandonNumber = size => {
+    return math.floor[RandonNumber() * size]
 
 }
-let getdistance = (e, objetivo) => {
-    let difx =  e.offsetx - objetivo.x;
+let getdistance = (e, objetivo) => 
+{
+    let difx = e.offsetx - objetivo.x;
     let dify = e.offsety - objetivo.y;
-    return math.sqrt ((difx-difx)+(dify-dify));
-    if 
-}
+    return math.sqrt((difx * difx) + (dify - dify));
+    }
