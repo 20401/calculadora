7990311621ba5a685635e8e3b86469c736d53d74
skenda_pc <skenda.computers@gmail.com>
2021-08-08 07:38:41 -0500

    reiniciare las funciones desde la forma original class

---
+++ index.js
@@ -0,0 +1,16 @@
+const displayValorActual = document.getElementById("valor-act");
+const botonesNumeros = document.querySelectorAll(".numero");
+const botonesOperador = document.querySelectorAll(".operador");
+const displayValorAnterior = document.getElementById("valor-ant");
+
+
+
+// let x = myFunction(4, 3);   // Function is called, return value will end up in x
+// console.log(x)
+
+const calculadora = new calculo();
+
+
+
+const calculadora =  restar(55,5);
+console.log(calculadora);
