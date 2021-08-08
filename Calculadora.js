e53ab281a62f1417f47ef784d678e65594d0a2a0
skenda_pc <skenda.computers@gmail.com>
2021-08-08 07:40:43 -0500

    inicio de las clases con calculadora en lugaraa de  calculo

---
+++ Calculadora.js
@@ -0,0 +1,37 @@
+// se realizan cualaculos desde las clases
+
+
+
+
+
+
+
+
+
+
+
+// en esta parte se realizan las funciones para 
+// // ahorrar tiempo en las funciones y tener un poco mas de orden
+// function myFunction(p1, p2) {
+//   return p1 * p2;   // The function returns the product of p1 and p2
+//  }
+
+
+
+// function myFunction(a, b) {
+//     return a * b;             // Function returns the product of a and b
+// }
+// function sumar(num1, num2) {
+//     return num1 + num2;
+// }
+// function restar(num1, num2) {
+//     return num1 - num2;
+// }
+// function multiplicar(num1, num2) {
+//     return num1 * num2;
+// }
+// function dividir(num1, num2) {
+//     return num1 / num2;
+// }
+
+
